5f0c9652ea1417b7f2f373fbe57636c794a5b978
smnrbln <semanur.belen@bil.omu.edu.tr>
2012-07-18 13:56:24 +0300

    first

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+ before_filter :authenticate, <img src='http://tuxleaks.org/wp-includes/images/smilies/icon_surprised.gif' alt=':o' class='wp-smiley' /> nly => :destroy
+ 
+  def create
+    @post = Post.find(params[:post_id])
+# kalabalık etmesin diye kısaltıldı
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummerOfProject::Application.config.secret_token = '7ca9abe7372d71d69048eb1280848cf922e1993fb2430d814905ca3f59839283eec42496d0a26a023dbec997719850f85677037ea194d7058a948e410553c943'
